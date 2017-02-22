9897cf227872593665f30376a43d7c0c5b549fca
Ryan <ryanpsuresh@gmail.com>
2017-02-21 20:55:45 -0600

    add speechToText functionality

--- app/speech.js
+++ app/speech.js
@@ -16,4 +16,26 @@ angular.module('app.speech', [])
     return true;
   }
 
-}
+    function createRecognition() {
+    var recognition = new webkitSpeechRecognition();
+    recognition.start();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.lang = "en-US";
+    recognition.maxAlternatives = 1;
+    return recognition;
+  }
+
+  function speechToText(recognition, data, callback) {
+    data.text = '';
+    recognition.onresult = function (event) {
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        data.text += event.results[i][0].transcript;
+      }
+      recognition.stop();
+      callback(data);
+    };
+  }
+
+
+});
