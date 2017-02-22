206ca2b06f3dba808d0bea38e46da1128ddebbc7
Ryan <ryanpsuresh@gmail.com>
2017-02-21 19:57:28 -0600

    add index.html file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 const parseURL = require('./parseURL.js');
 
