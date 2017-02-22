d9e9f14afb72541884c28906c01f4a81643d4cf9
Ryan <ryanpsuresh@gmail.com>
2017-02-22 15:14:51 -0600

    add readme

--- app/speech.js
+++ app/speech.js
@@ -37,16 +37,4 @@ angular.module('app.speech', [])
     };
   }
 
-  // function checkAsh(recognition) {
-  //   data.text = '';
-  //   recognition.onresult = function (event) {
-  //     for (var i = event.resultIndex; i < event.results.length; ++i) {
-  //       data.text += event.results[i][0].transcript;
-  //     }
-  //     recognition.stop();
-  //     callback(data);
-  //   };
-  // }
-
-
 });
